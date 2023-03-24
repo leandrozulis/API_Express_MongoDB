8e5e2d51f83660b248710e95a82ef114d6d1b629
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-03-24 16:28:55 -0300

    Criado as rotas de buscar por ID, atualizar e cadastrar

--- src/controllers/livrosControllers.js
+++ src/controllers/livrosControllers.js
@@ -2,6 +2,18 @@ import livros from "../models/Livro.js";
 
 class LivroController {
 
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id)
+      .then(livro => {
+        res.status(200).json(livro)
+      })
+      .catch(err => {
+        res.status(400).send({message: `${err.message} - ID do livro não localizado`})
+      })
+  }
+
   static listarLivros = async (req, res) => {
     try {
       const livrosResultado = await livros.find();
@@ -9,6 +21,29 @@ class LivroController {
     } catch (err) {
       res.status(500).json(err);
     }
+  }
+
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body);
+    livro.save()
+      .then(() => {
+        res.status(201).send(livro.toJSON())
+      })
+      .catch(err => {
+        res.status(500).send({message: `${err} - Falha ao cadastar livro.`})
+      })
+  }
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, {$set: req.body})
+    .then(() => {
+      res.status(200).send({message: 'Livro atualizado com sucesso!'})
+    })
+    .catch(err => {
+      res.status(500).send({message: err.message})
+    })
   }
 
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,9 @@ import LivroController from '../controllers/livrosControllers.js'
 const router = express.Router();
 
 router
-  .get('/livros', LivroController.listarLivros);
+  .get('/livros', LivroController.listarLivros)
+  .get('/livros/:id', LivroController.listarLivroPorId)
+  .post('/livros', LivroController.cadastrarLivro)
+  .put('/livros/:id', LivroController.atualizarLivro)
 
 export default router;
