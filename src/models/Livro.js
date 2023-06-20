9f1f1bb7e04b98d92586d49cdd01291eae426c6a
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-06-20 10:31:40 -0300

    validate validadtor message

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,7 +6,14 @@ const livroSchema = new mongoose.Schema(
     titulo: { type: String, required: [true, "O titulo do livro é obrigatório"] },
     autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O(a) autor(a) é obrigatório"] },
     editora: { type: String, required: [true, "A editora é obrigatória"], enum: {values: ["Casa do código", "Alura"], message: "A editora '{VALUE}' não é um valor permitido."} },
-    numeroPaginas: { type: Number, min: [10, "O número de páginas deve estar entra 10 e 5000. Valor fornecido: {VALUE}"], max: [5000, "O número de páginas deve estar entra 10 e 5000. Valor fornecido: {VALUE}"] }
+    numeroPaginas: { type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entra 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
